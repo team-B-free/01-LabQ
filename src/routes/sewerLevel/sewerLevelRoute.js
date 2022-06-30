00b16e53659a7480ec5e142ba7c1150c4d8f6e63
Woody-Park <starface1009@gmail.com>
2022-06-30 12:36:21 +0900

    [Feat] 공공 데이터 수집
        - 서울시 하수관로 수위 공공 데이터 수집
        - 서울시 강우량 정보 데이터 수집
        - 데이터 통합
    
        Feat/6

---
+++ src/routes/sewerLevel/sewerLevelRoute.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import * as controller from "../../controllers/sewerLevelController/sewerLevelController.js";
+const router = Router();
+
+router.get("/", controller.getApiData);
+
+export default router;
