2ef1167de53b0d0e31500c6c3521692fccbda313
KCH6937 <adasddasd12@naver.com>
2022-07-01 14:35:44 +0900

    [Refactor] 반환값 모듈 적용, 변수명 수정

--- src/modules/apiProcessing.js
+++ src/modules/apiProcessing.js
@@ -1,24 +1,20 @@
+import { responseRainGauge } from "../utils/responseData.js";
+
 const apiProcessing = (originalRainFall, originalSewerLevel) => {
-  const { GUBN, GUBN_NAM } = originalSewerLevel[0];
-  
-  const data = {};
-  data.GUBN = GUBN;
-  data.GUBN_NAM = GUBN_NAM;
+  const { GUBN: gubn, GUBN_NAM: gubnName } = originalSewerLevel[0];
 
   //한시간 하수관 수위 평균
-  const sewerLevelAvg = originalSewerLevel.reduce((prev, cur) => prev + cur.MEA_WAL, 0) / originalSewerLevel.length;
-  data.SEWER_LEVEL_AVG_PER_HOUR = sewerLevelAvg;
+  const sewerLevelAvg = originalSewerLevel.reduce((pre, cur) => pre + cur.MEA_WAL, 0) / originalSewerLevel.length;
 
   //동별 강우량(RAINGAUGE_CODE, RAINGAUGE_NAME, 1시간 평균 강우량, 1시간 누적 강우량)
   const filteredRainFall = [];
-  const rainGaugeCode = [...new Set(originalRainFall.map(item => item.RAINGAUGE_CODE))];
+  const rainGaugeCode = [...new Set(originalRainFall.map(item => item.RAINGAUGE_CODE)) ];
   rainGaugeCode.forEach(code => {
     filteredRainFall.push(originalRainFall.find(item => item.RAINGAUGE_CODE === code));
   });
 
-  data.RAINGAUGE = [];
-
-  const rainFallAvgArr= [];
+  const rainGauge = [];
+  const rainFallAvgArr = [];
   const rainFallSumArr = [];
 
   rainGaugeCode.forEach((code) => {
@@ -36,13 +32,15 @@ const apiProcessing = (originalRainFall, originalSewerLevel) => {
     delete item.RAINFALL10;
     delete item.RECEIVE_TIME;
 
-    data.RAINGAUGE.push({
+    rainGauge.push({
       RAINGAUGE_CODE: item.RAINGAUGE_CODE,
       RAINGAUGE_NAME: item.RAINGAUGE_NAME,
       RAINFALL_AVG_PER_HOUR: rainFallAvgArr[idx],
       RAINFALL_SUM_PER_HOUR: rainFallSumArr[idx]
     });
   });
+
+  const data = responseRainGauge(gubn, gubnName, sewerLevelAvg, rainGauge);
 
   return data;
 }
