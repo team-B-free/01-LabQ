c2b92378e40b20a6ef7da275d74abb65900e5ce4
KCH6937 <adasddasd12@naver.com>
2022-07-01 17:39:51 +0900

    [Refactor] CamelCase 통일

--- src/controllers/sewerLevelController/sewerLevelController.js
+++ src/controllers/sewerLevelController/sewerLevelController.js
@@ -1,15 +1,15 @@
 import { response, errResponse } from "../../utils/response.js";
 import statusCode from "../../utils/statusCode.js";
 import message from "../../utils/responseMessage.js"
-import getApiData from "../../modules/sewerlevelData.js";
+import getApiData from "../../modules/sewerLevelData.js";
 import apiProcessing from "../../modules/apiProcessing.js";
 
 const getData = async (req, res) => {
   const { guCode } = req.query;
 
   const rawData = await getApiData(guCode) ?? {};
-  const { sewerlevelInfo, rainfallInfo } = rawData;
-  const data = apiProcessing(rainfallInfo, sewerlevelInfo);
+  const { sewerLevelInfo, rainfall } = rawData;
+  const data = apiProcessing(rainfall, sewerLevelInfo);
 
   if (data) {
     return res
